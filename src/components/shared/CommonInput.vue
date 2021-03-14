db6385639b2333fa8827beec5e52c8fa16e54029
duro06 <andi.meka.025@gmail.com>
2021-03-14 15:33:11 +0700

    register page and common input

---
+++ src/components/shared/CommonInput.vue
@@ -0,0 +1,28 @@
+<template>
+  <q-input 
+    bottom-slots
+    :value="text"
+    @input="$emit('update:text', $event)"
+    :label="label"
+  >
+        <template v-slot:prepend>
+          <div class="text-blue-4 q-gutter-sm">
+            <q-icon :name="icon" />
+          </div>
+        </template>
+        <template v-slot:append>
+          <q-icon
+            v-if="text.length"
+            name="close"
+            @click="text = ''"
+            class="cursor-pointer"
+          />
+        </template>
+      </q-input>
+</template>
+
+<script>
+export default {
+  props:['text', 'icon', 'label']
+}
+</script>
--- src/pages/Register.vue
+++ src/pages/Register.vue
@@ -1,12 +1,50 @@
 <template>
   <q-page class="flex flex-center bg-light-blue-1">
 
-    
+    <q-card class="q-pa-md">
+      <q-card-section class="bg-yellow-3 text-primary text-center">
+        <div class="text-h6">Register</div>
+
+        <data-input
+          icon="fas fa-id-badge"
+          label="Nama"
+          :text.sync="registerData.name"
+        />
+
+        <email-input
+          icon="email"
+          label="e-mail"
+          :email.sync="registerData.email"
+        />
+
+        <password-input
+          icon="fas fa-lock"
+          label="password"
+          :password.sync="registerData.password"
+        />
+      </q-card-section>
+    </q-card>
 
   </q-page>
 </template>
 <script>
 export default {
-  name:'register'
+  name:'register',
+  components:{
+    'data-input':()=>import('components/shared/CommonInput'),
+    'email-input':()=>import('components/shared/EmailInput'),
+    'password-input':()=>import('components/shared/PasswordInput')
+  },
+  data(){
+    return{
+      registerData:{
+        email:'',
+        password:'',
+        name:''
+      },
+      loading:false,
+      disable:false
+    }
+  }
 }
 </script>
