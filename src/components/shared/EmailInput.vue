2f88d29c0c7e48460abb27f5586d9aac51b67cd2
duro06 <andi.meka.025@gmail.com>
2021-03-14 14:19:07 +0700

    make shared input, email and password

---
+++ src/components/shared/EmailInput.vue
@@ -0,0 +1,28 @@
+<template>
+  <q-input 
+    bottom-slots
+    :value="email"
+    @input="$emit('update:email', $event)"
+    :label="label"
+  >
+        <template v-slot:prepend>
+          <div class="text-blue-4 q-gutter-sm">
+            <q-icon :name="icon" />
+          </div>
+        </template>
+        <template v-slot:append>
+          <q-icon
+            v-if="email.length"
+            name="close"
+            @click="email = ''"
+            class="cursor-pointer"
+          />
+        </template>
+      </q-input>
+</template>
+
+<script>
+export default {
+  props:['email', 'icon', 'label']
+}
+</script>
---
+++ src/components/shared/PasswordInput.vue
@@ -0,0 +1,40 @@
+<template>
+  <q-input 
+    bottom-slots
+    :value="password"
+    @input="$emit('update:password', $event)"
+    :label="label"
+    :type="isPwd ? 'password' : 'text'"
+  >
+        <template v-slot:prepend>
+          <div class="text-blue-4 q-gutter-sm">
+            <q-icon :name="icon" />
+          </div>
+        </template>
+        
+        <template v-slot:append>
+          <q-icon
+            :name="isPwd ? 'visibility_off' : 'visibility'"
+            class="cursor-pointer"
+            @click="isPwd = !isPwd"
+          />
+          <q-icon
+            v-if="password.length"
+            name="close"
+            @click="password = ''"
+            class="cursor-pointer"
+          />
+        </template>
+      </q-input>
+</template>
+
+<script>
+export default {
+  props:['password', 'icon', 'label'],
+  data(){
+    return{
+      isPwd:true
+    }
+  }
+}
+</script>
